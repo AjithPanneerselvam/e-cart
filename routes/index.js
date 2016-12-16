21c2f3747888d44f56f96397700c8955a0328c59
Harish Kumar <saiharish120896@gmail.com>
2016-12-16 15:05:56 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Book-cart' });
+  var products = Product.find();
+  res.render('shop/index', { products:products });
 });
 
 module.exports = router;
