07415c5c7eae2b300e19bc8a2c93f8ca20ca8900
Harish Kumar <saiharish120896@gmail.com>
2016-12-16 15:18:58 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,16 @@ var Product = require('../models/product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var products = Product.find();
-  res.render('shop/index', { products:products });
+  Product.find(function(err,docs){
+    var productChunks = [];
+    var chunksize = 3;
+    for(var i = 0; i < docs.length ;i+= chunksize)
+    { 
+      productChunks.push(docs.slice(i,i+chunksize));
+    }
+  res.render('shop/index', { products:productChunks });
+  });
+  
 });
 
 module.exports = router;
